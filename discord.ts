ba216a9c2f2a62720ce5aff864d615170f6a6575
takumi <51111242+takumi3488@users.noreply.github.com>
2023-01-19 22:25:14 +0900

    Update discord.ts

--- discord.ts
+++ discord.ts
@@ -1,11 +1,6 @@
 import axios from "npm:axios@1.2.0";
 
 const discord = async (webhookUrl: string, text: string) => {
-  const token = Deno.env.get("LINE_TOKEN");
-  if (!token) {
-    console.error("環境変数`LINE_TOKEN'をセットしてください。");
-    return
-  }
   const res = await axios.post(
     webhookUrl,
     {
